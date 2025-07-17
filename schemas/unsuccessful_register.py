0b2d444583fd74854c9824542c138381771a3c0c
Maks747 <valol93@mail.ru>
2025-07-17 11:38:44 +0300

    Добавил схемы

---
+++ schemas/unsuccessful_register.py
@@ -0,0 +1,12 @@
+unsuccessful_register = {
+    "$schema": "http://json-schema.org/draft-04/schema#",
+    "type": "object",
+    "properties": {
+        "error": {
+            "type": "string"
+        }
+    },
+    "required": [
+        "error"
+    ]
+}
